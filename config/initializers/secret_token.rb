6823a25bbe7688dc9cc07f97dbc19fdd60bc6761
Likun Liu <likun.me@gmail.com>
2011-03-09 22:44:00 +1100

    proj set up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tsds::Application.config.secret_token = '169b95c69c3f31456c967efdab53857ee38409ee43725c8575fb62c5ae1924e1c852ddb0f727c36517ab8dec4ecd9a9ab85bbe365a86e9e716d17c9122b8f6d6'
