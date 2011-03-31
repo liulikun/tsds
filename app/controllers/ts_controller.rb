d4c7d51fd38f04938478f4de50856dfd899450af
Likun Liu <likun.liu@rea-group.com.au>
2011-04-01 00:10:47 +1100

    added ajax, updated the routing

--- app/controllers/ts_controller.rb
+++ app/controllers/ts_controller.rb
@@ -1,51 +1,48 @@
 class TsController < ApplicationController
 
-  def new
-    p '========='
-    p params
-
-    render :new and return unless params['ts']
-
+  def create
     address = params['ts']['address']
 
     url = 'http://maps.googleapis.com/maps/api/geocode/json?address=' + URI.escape(address) + '&sensor=false'
     resp = Net::HTTP.get_response(URI.parse(url))
     result = JSON.parse(resp.body)
-    p 'xxxxxxxx'
-    p result
+    p "GEOCODING=#{result}"
 
-    lat = result['results'][0]['geometry']['location']['lat']
-    long = result['results'][0]['geometry']['location']['lng']
+    location = result['results'][0]['geometry']['location']
+    lat = location['lat']
+    long = location['lng']
 
-#    p "#{lat},#{long}"
+    direct_distances = calculate_direct_distances(lat, long)
 
-    direct_distances = {}
-    TrainStation::TM.each do |station, geocode|
-      slat, slong = geocode.split(',')
+    @result = calculate_nearest_station(direct_distances, lat, long)
+
+    render :result
+  end
 
 
-      d = (slat.to_f - lat)**2 + (slong.to_f - long)**2
-#      p "#{station}=#{d}"
-      direct_distances[station] = d
-
-#      {tooltipHtml:" (3.6\x26#160;km / 6 mins)",polylines:[
+  def calculate_direct_distances lat, long
+    returning Hash.new do |result|
+      TrainStation::TM.each do |station, geocode|
+        slat, slong = geocode.split(',')
+        distance = (slat.to_f - lat)**2 + (slong.to_f - long)**2
+        result[station] = distance
+      end
     end
+  end
 
+  def calculate_nearest_station direct_distances, lat, long
     shortest_walk_distance = 9999999
     nearest_station = ''
 
     direct_distances.sort { |a, b| a[1] <=> b[1] }[0..1].each { |elem|
-      u = "http://maps.google.com.au/maps?gl=au&dirflg=w&output=dragdir&saddr=#{lat},#{long}&daddr=#{TrainStation::TM[elem[0]]}"
-      resp = Net::HTTP.get_response(URI.parse(u))
+      url = "http://maps.google.com.au/maps?gl=au&dirflg=w&output=dragdir&saddr=#{lat},#{long}&daddr=#{TrainStation::TM[elem[0]]}"
+      resp = Net::HTTP.get_response(URI.parse(url))
 
       html = resp.body
-
-
       html =~ /.*(\(([0-9\.]+).*;(.*)\/.*\)).*/
-      candidate = "#{elem[0]} =~ #{$2.strip} #{$3.strip}"
-#      puts candidate
+      candidate = "#{elem[0]} =~ #{$2} #{$3}"
 
-      candidate_distance = $3.strip == 'km' ? ($2.strip.to_f * 1000).to_i : $2.strip.to_i
+      candidate_distance = $3.strip == 'km' ? ($2.to_f * 1000).to_i : $2.to_i
 
       if candidate_distance < shortest_walk_distance
         shortest_walk_distance = candidate_distance
@@ -53,11 +50,7 @@ class TsController < ApplicationController
       end
     }
 
-    puts nearest_station
-
-    @result = nearest_station
-    
-    render :result
+    nearest_station
   end
 
 end
