229c74463cd9879cbfa4bf112476dd435ec1cada
Likun Liu <likun.liu@rea-group.com.au>
2011-04-01 11:01:31 +1100

    moved the calculation to model

--- app/controllers/ts_controller.rb
+++ app/controllers/ts_controller.rb
@@ -9,48 +9,10 @@ class TsController < ApplicationController
     p "GEOCODING=#{result}"
 
     location = result['results'][0]['geometry']['location']
-    lat = location['lat']
-    long = location['lng']
 
-    direct_distances = calculate_direct_distances(lat, long)
-
-    @result = calculate_nearest_station(direct_distances, lat, long)
+    @result = TrainStation.calculate_nearest_station(location['lat'], location['lng'])
 
     render :result
-  end
-
-
-  def calculate_direct_distances lat, long
-    returning Hash.new do |result|
-      TrainStation::TM.each do |station, geocode|
-        slat, slong = geocode.split(',')
-        distance = (slat.to_f - lat)**2 + (slong.to_f - long)**2
-        result[station] = distance
-      end
-    end
-  end
-
-  def calculate_nearest_station direct_distances, lat, long
-    shortest_walk_distance = 9999999
-    nearest_station = ''
-
-    direct_distances.sort { |a, b| a[1] <=> b[1] }[0..1].each { |elem|
-      url = "http://maps.google.com.au/maps?gl=au&dirflg=w&output=dragdir&saddr=#{lat},#{long}&daddr=#{TrainStation::TM[elem[0]]}"
-      resp = Net::HTTP.get_response(URI.parse(url))
-
-      html = resp.body
-      html =~ /.*(\(([0-9\.]+).*;(.*)\/.*\)).*/
-      candidate = "#{elem[0]} =~ #{$2} #{$3}"
-
-      candidate_distance = $3.strip == 'km' ? ($2.to_f * 1000).to_i : $2.to_i
-
-      if candidate_distance < shortest_walk_distance
-        shortest_walk_distance = candidate_distance
-        nearest_station = candidate
-      end
-    }
-
-    nearest_station
   end
 
 end
--- app/models/train_station.rb
+++ app/models/train_station.rb
@@ -264,9 +264,6 @@ class TrainStation
       'North Williamstown'
   ]
 
-  TM1 = {
-      'Aircraft' => '-37.8666572,144.7608068'
-  }
 
   TM = {
       'Aircraft' => '-37.8666572,144.7608068',
@@ -480,4 +477,41 @@ class TrainStation
       'Yarraman' => '-37.9779189,145.1913499',
       'Yarraville' => '-37.8155649,144.8899918'
   }
+
+  def self.calculate_direct_distances lat, long
+    returning Hash.new do |result|
+      TM.each do |station, geocode|
+        slat, slong = geocode.split(',')
+        distance = (slat.to_f - lat)**2 + (slong.to_f - long)**2
+        result[station] = distance
+      end
+    end
+  end
+
+  def self.calculate_nearest_station lat, long
+
+    shortest_walk_distance = 9999999
+    nearest_station = ''
+
+    direct_distances = calculate_direct_distances(lat, long)
+    direct_distances.sort { |a, b| a[1] <=> b[1] }[0..1].each { |elem|
+      url = "http://maps.google.com.au/maps?gl=au&dirflg=w&output=dragdir&saddr=#{lat},#{long}&daddr=#{TrainStation::TM[elem[0]]}"
+      resp = Net::HTTP.get_response(URI.parse(url))
+
+      html = resp.body
+      html =~ /.*(\(([0-9\.]+).*;(.*)\/.*\)).*/
+      candidate = "#{elem[0]} =~ #{$2} #{$3}"
+
+      candidate_distance = $3.strip == 'km' ? ($2.to_f * 1000).to_i : $2.to_i
+
+      if candidate_distance < shortest_walk_distance
+        shortest_walk_distance = candidate_distance
+        nearest_station = candidate
+      end
+    }
+
+    nearest_station
+  end
+
+
 end
