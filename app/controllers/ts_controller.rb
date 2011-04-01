399518549325074d53ef792162994370fc4dc0aa
Likun Liu <likun.liu@rea-group.com.au>
2011-04-01 12:15:40 +1100

    jsonp support

--- app/controllers/ts_controller.rb
+++ app/controllers/ts_controller.rb
@@ -1,18 +1,33 @@
 class TsController < ApplicationController
 
+  respond_to :html, :json
+
+  def show
+    address = params['address']
+    if address
+      location = geo_location(address)
+      station = TrainStation.calculate_nearest_station(location['lat'], location['lng'])
+
+      respond_with(@result = "#{params['callback']}({station:'#{station}'})")
+    end
+  end
+
   def create
     address = params['ts']['address']
+    location = geo_location(address)
+    @result = TrainStation.calculate_nearest_station(location['lat'], location['lng'])
 
+    render :result
+  end
+
+  def geo_location(address)
     url = 'http://maps.googleapis.com/maps/api/geocode/json?address=' + URI.escape(address) + '&sensor=false'
     resp = Net::HTTP.get_response(URI.parse(url))
     result = JSON.parse(resp.body)
     p "GEOCODING=#{result}"
 
-    location = result['results'][0]['geometry']['location']
+    result['results'][0]['geometry']['location']
 
-    @result = TrainStation.calculate_nearest_station(location['lat'], location['lng'])
-
-    render :result
   end
 
 end
