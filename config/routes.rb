d9f0640f92620096a0d2236de2bfdba00bf09825
Likun Liu <likun.liu@rea-group.com.au>
2011-05-04 21:38:06 +1000

    root route

--- config/routes.rb
+++ config/routes.rb
@@ -48,7 +48,7 @@ Tsds::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-#  root :to => "welcome#index"
+  root :to => "ts#show"
   resource 'ts'
 
   # See how all your routes lay out with "rake routes"
